f35f2be78358e790f109f004bd686c77119fbdf7
NaveedAfraz <naveedafraz123@gmail.com>
2025-02-21 20:11:30 +0530

    lastest stuff

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -114,19 +114,5 @@ router.get("/get-chatList/:userId", async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
-router.post("/new-route", async (req, res) => {
-  const { param1, param2 } = req.body;
-  try {
-    if (!param1 || !param2)
-      return res.status(400).json({ message: "something is missing" });
-
-    // Add your logic here
-
-    return res.status(200).json({ message: "New route created successfully" });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-});
 
 module.exports = router;
