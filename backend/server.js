32dcf9c9c16ce1035ca93784e82b292ef7513ae3
Naveed Afraz <140966371+NaveedAfraz@users.noreply.github.com>
2025-07-18 16:09:41 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use("/api", aiRouter);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
